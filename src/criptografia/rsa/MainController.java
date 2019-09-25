23d8d933d8bb510abd2b8cb18fa5687c24e1b383
hiarlyfs <hiarlyfsouto@ccc.ufcg.edu.br>
2019-09-24 21:33:49 -0300

    Adiciona especificacoes

--- src/criptografia/rsa/MainController.java
+++ src/criptografia/rsa/MainController.java
@@ -5,25 +5,33 @@
  */
 package criptografia.rsa;
 
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.ResourceBundle;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 /**
  *
  * @author italomcdp
  */
-public class MainController {
+public class MainController implements Initializable{
     
     private int q;
     private int p;
     private static int z;
     private static int n;
+    private int d;
+    private int e;
+    private int valE;
     
     @FXML
     private TextField textFieldNum1;
@@ -36,6 +44,34 @@ public class MainController {
     
     @FXML
     private ComboBox<Integer> listaCoPrimos;
+    
+    @FXML
+    private Label labelInversoModular;
+    
+    @FXML
+    private TextField textFielValorK;
+    
+    @FXML
+    private Button buttonOkValorK;
+    
+    @FXML
+    private Label labelChavePublica;
+    
+    @FXML
+    private Label labelChavePrivada;
+    
+    @FXML
+    private TextField TextFieldPalavra;
+    
+    @FXML
+    private Button buttonSelecionarPalavra;
+    
+    @FXML
+    private Label labelRecebido;
+    
+    @FXML
+    private Label labelDescriptografado;
+    
     
     @FXML
     public void GerarCoPrimosAction(ActionEvent event) {
@@ -61,6 +97,17 @@ public class MainController {
         return lista;
     }
     
+    private int inversoModular(int valor, int modulo) {
+    	int contador = 1;
+    	while (true) {
+    		if ((valor * contador) % modulo == 1) {
+    			return contador;
+    		} else {
+    			contador += 1;
+    		}
+    	}
+    }
+    
     private static int mdc(int a, int b) {
         while (b != 0) {
             int resto = a % b;
@@ -70,7 +117,84 @@ public class MainController {
         
         return a;
     }
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		listaCoPrimos.valueProperty().addListener((ov, oldValue, newValue) -> {
+			if (!newValue.equals("")) {
+				d = newValue;
+				labelChavePrivada.setText("(" + d + ", " + z + ")");
+				e = inversoModular(newValue, n);
+				labelInversoModular.setText("Chave Privada = " + e + " + k." + n + "\ncom k"
+						+ " pertencendo aos inteiros");
+			} else {
+				System.out.println(newValue);
+			}
+		});
+	}
+	
+	@FXML
+	public void onBtValorKAction(ActionEvent event) {
+		int valorK = Integer.parseInt(textFielValorK.getText());
+		int valorE = e + valorK*n;
+		valE = valorE;
+		System.out.println(d);
+		System.out.println(valE);
+		labelChavePublica.setText("(" + valE + ", " + z + ")");
+		System.out.println(criptografando("teste"));
+	}
     
+	private ArrayList<Integer> codigoPalavra(String palavra) {
+		ArrayList<Integer> lista = new ArrayList<Integer>();
+		for (int i = 0; i < palavra.length(); i++) {
+			lista.add((int) palavra.charAt(i) - 96);
+		}
+		
+		return lista;
+	}
+	
+	private ArrayList<Integer> criptografando(String palavra) {
+		ArrayList<Integer> valores = codigoPalavra(palavra);
+		ArrayList<Integer> lista = new ArrayList<Integer>();
+		for (int num : valores) {
+			double valor = Math.pow(num, d);
+			int codigo =  (int) (valor % z);
+			lista.add(codigo);
+		}
+		System.out.println(lista);
+		return lista;
+	}
+	
+	private String descriptografando(ArrayList<Integer> valores) {
+		String palavra = "";
+		for (int num : valores) {
+			Long valor = (long) valE;
+			System.out.println(valE);
+			for (int i = 1; i <= z; i++) {
+				valor = valor*valE;
+			}
+			System.out.println(valor);
+			int codigo =  (int) (valor % z);
+			System.out.println(codigo);
+			palavra += (char) (codigo + 96);
+		}
+		return palavra;
+	}
+	
+	@FXML
+	public void onBtSelecionarPalavra(ActionEvent event) {
+		String palavraString = TextFieldPalavra.getText();
+		ArrayList<Integer> valoresCrptografados = criptografando(palavraString);
+		String palavraDescriptografa = descriptografando(valoresCrptografados);
+		
+		String valoresCripString = "";
+		for (int num : valoresCrptografados) {
+			valoresCripString += num + " ";
+		}
+		
+		labelRecebido.setText(valoresCripString);
+		labelDescriptografado.setText(palavraDescriptografa);
+	}
     
     
 }
